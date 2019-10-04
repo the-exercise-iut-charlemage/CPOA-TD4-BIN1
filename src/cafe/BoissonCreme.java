6b150b44d5463fbb1c4c6845aea9221e84efb71b
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-10-04 17:11:12 +0200

    add Deca, BoissonCreme

---
+++ src/cafe/BoissonCreme.java
@@ -0,0 +1,8 @@
+package cafe;
+
+public class BoissonCreme extends DecorateurIngredient{
+
+    public BoissonCreme(Boisson c) {
+        super(.55, "Crème", c);
+    }
+}
---
+++ src/cafe/Deca.java
@@ -0,0 +1,9 @@
+package cafe;
+
+public class Deca extends Boisson {
+
+    @Override
+    public double cout() {
+        return 1;
+    }
+}
